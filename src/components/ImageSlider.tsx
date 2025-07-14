182854cb072728fc6e0c7a3a5d1d5290aa45b201
Olawoyin <yustee2017@gmail.com>
2025-07-14 15:54:56 +0100

    done

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -5,9 +5,9 @@ const ImageSlider: React.FC = () => {
  
   return (
     <div className="w-full relative  mx-auto mb-8">
-        <div className='absolute w-full md:h-[500px] h-[350px] border border-gray-700 rounded-xl -left-1 top-1 md:-left-4 md:top-3 z-20'/>
+        <div className='absolute w-full md:h-[500px] h-[390px] border border-gray-700 rounded-xl -left-1 top-1 md:-left-4 md:top-3 z-20'/>
       
-      <div className="w-full  h-[350px] md:h-[500px] object-cover flex items-center justify-center overflow-hidden rounded-lg z-50" >
+      <div className="w-full  h-[390px] md:h-[500px] object-cover flex items-center justify-center overflow-hidden rounded-lg z-50" >
         <img src="../../Images/hero3.webp" alt="" className='object-cover w-full h-full' />
       </div>
     </div>
